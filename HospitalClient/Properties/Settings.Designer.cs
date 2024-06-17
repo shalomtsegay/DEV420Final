bd8ea914cdc4c1bb3859cb09b0f841a5d9d8d677
Lyrenzo <112978336+Lyrenzo@users.noreply.github.com>
2024-06-17 12:07:20 -0700

    Add files via upload
    Appointment Scheduling

--- HospitalClient/Properties/Settings.Designer.cs
+++ HospitalClient/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HospitalClient.Properties
-{
-
-
+namespace HospitalClient.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=desktop-17tntl7\\sqlexpress;Initial Catalog=HospitalInventoryDB;Integr" +
+            "ated Security=True;TrustServerCertificate=True")]
+        public string HospitalInventoryDBConnectionString {
+            get {
+                return ((string)(this["HospitalInventoryDBConnectionString"]));
             }
         }
     }
